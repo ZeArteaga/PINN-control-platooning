54432c35ef70806ef497713a695af0defc07f57f
Zé <zearteaga@gmail.com>
2025-06-30 16:11:42 +0100

    Implemented sliding window in do mpc. Not sure if I should have the integral action state in the sim itself. Anyway, it is running. Third order model had a bug (u was not being divided by mass for acc conversion).

--- src/mpc/main.py
+++ src/mpc/main.py
@@ -1,33 +1,111 @@
 import os
+from collections import deque
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from do_mpc.simulator import Simulator
 from do_mpc.controller import MPC
-from modelling import SecondOrderPINNmodel, SecondOrderIdeal
+from modelling import SecondOrderPINNmodel, SecondOrderIdeal, ThirdOrderModel, DiscreteWindowedPINN
 from controller import setupDMPC, setupSim
 from plotting import setup_graphics, plot
 from do_mpc.data import save_results
 class FV:
-     def __init__(self, fv_initial: np.ndarray, mpc: MPC, sim: Simulator):
-        self.state = fv_initial
+    def __init__(self, mpc: MPC, sim: Simulator, mass: float, window_size: int):
+        '''Window size including current state. e.g: history of 2 steps -> window_size = 3'''
         self.mpc = mpc
         self.sim = sim
-        self.mpc.x0 = fv_initial
-        self.sim.x0 = fv_initial
-        self.mpc.set_initial_guess()
+        self.mass = mass
+        self.window_size = window_size
+        self.sliding_window = deque(maxlen=window_size-1)
 
-     def set_graphics(self, mpc_graphics, sim_graphics):
+    def set_initial_conditions(self, x0: dict):
+        """Set initial conditions for both MPC and simulator"""
+        # The control input u is a scalar. Initialize it.
+        self.u = x0['u0'] 
+        
+        # Initialize sliding window with repeated initial values
+        # The window contains [t, v, u] for past steps. u is a scalar.
+        initial_entry = [x0['t0'], x0['v0'], self.u] 
+        for _ in range(self.window_size - 1):
+            self.sliding_window.append(initial_entry)
+        
+        # mpc_state now holds the core states [x, v, Ie] that will be updated each step
+        self.mpc_state = np.array([x0['x0'], x0['v0'], x0['Ie0']])
+        
+        # Set initial conditions for do_mpc objects
+        self.sim.x0 = np.array([x0["x0"], x0["v0"], x0['Ie0'], x0["a0"]])
+        self.mpc.x0 = self.extend_mpc_state(self.mpc_state)
+        
+        self.mpc.set_initial_guess()
+        
+    def extend_mpc_state(self, mpc_state) -> np.array:
+        """
+        Combines the current core state [x, v, Ie] with the historical window
+        to form the full state vector required by the do_mpc model.
+        Order: [x, v, Ie, t_km1, v_km1, u_km1, t_km2, v_km2, u_km2, ...]
+        """
+        extended = list(mpc_state)
+        for entry in self.sliding_window:
+            extended.extend(entry)
+            
+        return np.array(extended)
+
+    def _update_mpc_window(self, t: float):
+        """Updates the sliding window with the latest measurement."""
+        v = self.mpc_state[1]
+        
+        new_entry = [t, v, float(self.u)]
+        self.sliding_window.appendleft(new_entry)
+
+    def mpc_step(self):
+        """Runs one step of the MPC controller."""
+        full_state = self.extend_mpc_state(self.mpc_state)
+        self.u = self.mpc.make_step(full_state)
+
+    def sim_step(self, t: float):
+        self.sim_state = self.sim.make_step(self.u).reshape((-1,))
+        self.mpc_state = self.sim_state[:-1] #ignore acceleration
+        self._update_mpc_window(t)
+
+    def set_graphics(self, mpc_graphics, sim_graphics):
         self.mpc_graphics = mpc_graphics
         self.sim_graphics = sim_graphics
 
+    def print_latest_mpc(self):
+        mpc_x = self.mpc.data['_x', 'x'][-1]
+        mpc_x_prec = self.mpc.data['_tvp', 'x_prec'][-1]
+        mpc_v = self.mpc.data['_x', 'v'][-1]
+        mpc_v_prec = self.mpc.data['_tvp', 'v_prec'][-1]
+        mpc_gap = self.mpc.data['_aux', 'd'][-1]
+        mpc_desired_gap = self.mpc.data['_aux', 'd_ref'][-1]
+        mpc_u = self.mpc.data['_u', 'u'][-1]
+
+        print(f"  Ego position (x): {mpc_x}")
+        print(f"  Ego velocity (v): {mpc_v}")
+        print(f"  Preceeding position (x_prec): {mpc_x_prec}")
+        print(f"  Preceeding velocity (v_prec): {mpc_v_prec}")
+        print(f"  Desired gap (d_ref): {mpc_desired_gap}")
+        print(f"  Actual gap (d): {mpc_gap}")
+        print(f"  Computed input acceleration (u/m): {mpc_u/self.mass}")
+
+    def print_latest_sim(self):
+        sim_x = self.sim.data['_x', 'x'][-1]
+        sim_x_prec = self.sim.data['_tvp', 'x_prec'][-1]
+        sim_v = self.sim.data['_x', 'v'][-1]
+        sim_v_prec = self.sim.data['_tvp', 'v_prec'][-1]
+        sim_gap = self.sim.data['_aux', 'd'][-1]
+        
+        print(f"  Ego position (x): {sim_x}")
+        print(f"  Ego velocity (v): {sim_v}")
+        print(f"  Preceeding position (x_prec): {sim_x_prec}")
+        print(f"  Preceeding position (v_prec): {sim_v_prec}")
+        print(f"  Actual gap (d): {sim_gap}\n")
+        
 if __name__ == "__main__":
     #*CONFIG---
-    VEHICLE_MASS_KG = 1500
-    h = 1
-    d_min = 2
-    L_prec = 4.5 #same len for every CAV
+    VEHICLE_MASS = 1500
     dt = 0.1
+    L_prec = 4.5
     t_samp = np.array([0, 5, 10, 15, 20]) #time check points
     t_end = t_samp[-1]
     noise_std = 0 #TODO
@@ -35,27 +113,46 @@ if __name__ == "__main__":
     #TODO: add driving cycle for leader
     lv_samp = np.array([50, 50, 50, 50, 50]) / 3.6 #leader speed check points 
     lv_speed = interp1d(t_samp, lv_samp, kind='quadratic') #quadratic interpolation -> no drivetrain limitation for now
-    lv_x0 = 200
+    lv_x0 = 30 #start at arbitrary position
     lv_curr_state = { #initial state - Leader Vehicle
-        'x': lv_x0,
+        'x': lv_x0, 
         'v': lv_speed(0)
     }
 
-    def get_lv_state(t_now):
-        x,v = lv_curr_state['x'], lv_curr_state['v'] 
-        #print(x,v)
-        return x,v
+    def fn_get_prec_state(platoon: list, vehicle_idx: int):
+        if vehicle_idx == 0:
+            # For the first follower, the preceding vehicle is the leader.
+            x_prec = lv_curr_state['x']
+            v_prec = lv_curr_state['v']
+        else:
+            # For other followers, the preceding vehicle is in the platoon list.
+            prec = platoon[vehicle_idx - 1]
+            x_prec = prec.state[0]
+            v_prec = prec.state[1]
+
+        return x_prec, v_prec
 
     #Followers
     ini_gap = L_prec + 20 
-    fv_v0 = 40/3.6
-    fv_initials = [np.array([lv_x0 - ini_gap, fv_v0])]
+    fv0_ini = {
+        "x0": lv_x0 - ini_gap,
+        "v0": 40/3.6,
+        'Ie0': 0, #integral action
+        'a0': 0,
+        'u0': 0,
+        't0': 0
+    }
+    fv_initials = [fv0_ini] #x, v, 0 integral action, 0 acceleration
     platoon_size = len(fv_initials) #excluding leader
     #will use the following to instantiate various decentralized MPC for each CAV
-    model_params = {'h': h,
-                    'd_min': d_min,
-                    'L_prec': L_prec,
-                    'm': VEHICLE_MASS_KG}
+    model_params = {'h': 1,
+                    'd_min': 2,
+                    'L_prec': 4.5,
+                    'm': VEHICLE_MASS,
+                    'tau': 0.5,
+                    'window_size': 3,
+                    'dt': dt
+                    }
 
     mpc_config = {
             'n_horizon': 10,
@@ -64,12 +161,17 @@ if __name__ == "__main__":
             'store_full_solution': True,
             'collocation_deg': 2, #default 2nd-degree polynomial to approximate the state trajectories
             'collocation_ni': 1, #default
+            'nlpsol_opts': {'ipopt.linear_solver': 'MA27',
+                            'ipopt.print_level':0, 'print_time':0}
+            
             }
     
     opt_params = {
-        'Q': np.diag([100, 10]),
-        'P': 100,
-        'R': 0.0001,
+        'Q': np.diag([1000, 10, 100]), #PDI [100, 10, 100]
+        'P': np.diag([0, 0, 0]), #TODO: INvestigate terminal cost
+        'R': 0.1,
+        'u_max': 5*VEHICLE_MASS,
+        'u_min': -8*VEHICLE_MASS,
         #TODO: Realistic Constraints
     }
 
@@ -82,90 +184,65 @@ if __name__ == "__main__":
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
 
-    pinn_model_path = os.path.join(script_dir, "../models/onnx/pinn_model_udds_10%.onnx")
-    scalerX_path = os.path.join(script_dir, "../models/scalers/scalerX_model_udds_10%.save")
-    scalerY_path = os.path.join(script_dir, "../models/scalers/scalerY_model_udds_10%.save")
-    results_path = os.path.join(script_dir, ".results/")
-
+    pinn_model_path = os.path.join(script_dir, "../../models/onnx/pinn_FC_udds_hwycol_70%_windowsize3.onnx")
+    scalerX_path = os.path.join(script_dir, "../../models/scalers/scalerX_FC_udds_hwycol_70%_windowsize3.save")
+    scalerY_path = os.path.join(script_dir, "../../models/scalers/scalerY_FC_udds_hwycol_70%_windowsize3.save")
     # Building platoon...
     #same model for every vehicle (homogeneous platoon)
-    mpc_model = SecondOrderPINNmodel(pinn_model_path, model_params,
+    mpc_model = DiscreteWindowedPINN(pinn_model_path, model_params,
                                      scalerX_path=scalerX_path,
                                      scalerY_path=scalerY_path)
-    plant_model = SecondOrderIdeal(model_params)
-    print("Pinn model control input and states:", mpc_model.u.keys(), mpc_model.x.keys())
-    print("Pinn model time varying parameters (provided):", mpc_model.tvp.keys())
-
+    plant_model = ThirdOrderModel(model_params)
+    print("Pinn (MPC) model control input and states:", mpc_model.u.keys(), mpc_model.x.keys())
+    print("Pinn (MPC) model time varying parameters (provided):", mpc_model.tvp.keys())
+    print("Plant model control input and states:", plant_model.u.keys(), plant_model.x.keys())
+    print("Plant model time varying parameters (provided):", plant_model.tvp.keys())
+    
     platoon = []
     for i in range(0, platoon_size):
         print(f"Setting MPC for follower {i}...")
-        if i==0:
-            fn_get_prec = get_lv_state #set leader function
-        else:
-            #! not implemented
-            raise NotImplementedError("Setup for followers beyond the leader is not implemented yet.")
-        
-        mpc = setupDMPC(mpc_model, mpc_config, opt_params, fn_get_prec)
-        mpc.settings.set_linear_solver(solver_name='MA27') #boosts speed supposedly
+
+        mpc = setupDMPC(mpc_model, mpc_config, opt_params, fn_get_prec_state, platoon, i)
         print("\n", mpc.settings)
-
-        sim = setupSim(plant_model, sim_config, get_prec_state=fn_get_prec)
-        platoon.append(FV(fv_initials[i], mpc, sim)) #create follower and add to platoon
-
-    #test:
-    for i, fv in enumerate(platoon):
-        print(f"\n(FV{i}) Set Initial Conditions:")
-        print(str(fv.mpc.x0['x']) + "m", str(fv.mpc.x0['v']*3.6) + "km/h")
+        sim = setupSim(plant_model, sim_config, fn_get_prec_state, platoon, i)
+        fv = FV(mpc, sim, VEHICLE_MASS, model_params['window_size'])
+        #MPC init conditions
+        fv_ini = fv_initials[i]
+        fv.set_initial_conditions(fv0_ini)
+        print(f"\n(FV{i}) MPC initial conditions: {fv.mpc.x0}")
+        print(f"\n(FV{i}) SIM (actual) initial conditions: {fv.sim.x0}")
+        platoon.append(fv) #add to platton        
+        
+        #for plotting
         mpc_graphics, sim_graphics = setup_graphics(fv.mpc.data, fv.sim.data)
         fv.set_graphics(mpc_graphics, sim_graphics)
-        
-        
+
     #Control loop:
     fv0 = platoon[0]
-    fv0.mpc.settings.supress_ipopt_output()
     for i, t_value in enumerate(t):
-        u = fv0.mpc.make_step(fv0.state)
-        #!DEBUG
-        """ print(f"DEBUG: MPC state at t={t_value:.1f}")
-        mpc_x = fv0.mpc.data['_x', 'x'][-1]
-        mpc_x_prec = fv0.mpc.data['_tvp', 'x_prec'][-1]
-        mpc_v = fv0.mpc.data['_x', 'v'][-1]
-        mpc_v_prec = fv0.mpc.data['_tvp', 'v_prec'][-1]
-        mpc_gap = fv0.mpc.data['_aux', 'd'][-1]
-        
-        print(f"  Ego position (x): {mpc_x}")
-        print(f"  Ego velocity (v): {mpc_v}")
-        print(f"  Preceeding position (x_prec): {mpc_x_prec}")
-        print(f"  Preceeding position (v_prec): {mpc_v_prec}")
-        print(f"  Calculated gap: {mpc_x_prec - mpc_x - L_prec}", f"using {fv0.mpc.model.aux['d']}")
-        print(f"  Actual gap (d): {mpc_gap}") """
+        fv0.mpc_step()
+        #! DEBUG
+        print(f"DEBUG: at t={t_value:.1f}")
+        fv0.print_latest_mpc()
+        #!---
 
         #* Have to do update of tvp before calling sim,
         #* for some reason simulator does all calculation before updating tvp, except for t=0 obv.
         #* The controller does the opposite.
         #* That's why there was a problem with the calculated gap vs the last actual gap calculated by the sim,
-        #* because this last one was stored using the previous leader position. So the update has to be in the middle to sync the two,
+        #* because this last one was stored using the previous leader position. So the update has 
+        #* to be in the middle to sync the two,
         #* and eliminate the discrepancy that was causing a steady-state error. 
 
-        lv_curr_state['v'] = lv_speed(t_value) 
-        lv_curr_state['x'] += lv_curr_state['v'] * dt
-        fv0.state = fv0.sim.make_step(u)
+        lv_curr_state['v'] = lv_speed(t_value)
+        lv_curr_state['x'] += lv_curr_state['v']*dt
+
+        fv0.sim_step(t_value)
         #!DEBUG
-        """ print(f"DEBUG: Simulator state at t={t_value:.1f}")
-        sim_x = fv0.sim.data['_x', 'x'][-1]
-        sim_x_prec = fv0.sim.data['_tvp', 'x_prec'][-1]
-        sim_v = fv0.sim.data['_x', 'v'][-1]
-        sim_v_prec = fv0.sim.data['_tvp', 'v_prec'][-1]
-        sim_gap = fv0.sim.data['_aux', 'd'][-1]
-        
-        print(f"  Ego position (x): {sim_x}")
-        print(f"  Ego velocity (v): {sim_v}")
-        print(f"  Preceeding position (x_prec): {sim_x_prec}")
-        print(f"  Preceeding position (v_prec): {sim_v_prec}")
-        print(f"  Calculated gap: {sim_x_prec - sim_x - L_prec}", f"using {fv0.sim.model.aux['d']}")
-        print(f"  Actual gap (d): {sim_gap}\n") """
-    
-    save_results([fv0.mpc, fv0.sim], overwrite=True, result_path=results_path)
+        #fv0.print_latest_sim()
+        #!---
+
+    save_results([fv0.mpc, fv0.sim], overwrite=True)
 
     fv0.mpc_graphics.plot_predictions(t_ind=0) #Additionally exemplify predictions
     plot(fv0.sim_graphics, name="(sim)")
--- src/mpc/modelling.py
+++ src/mpc/modelling.py
@@ -1,4 +1,5 @@
 import onnx
+import numpy as np
 import joblib
 import casadi as ca 
 from do_mpc.sysid import ONNXConversion
@@ -8,42 +9,46 @@ def _set_model_common_params(model, const_params: dict):
     """
     Adds to the basic model structure with common variables and parameters.
     """
-    h = const_params["h"]
-    d_min = const_params["d_min"]
+    model.set_variable('_u', 'u', shape=(1, 1))
+    model.set_variable('_x', 'x', shape=(1, 1)) #assuming 1D scenario else this is isn't needed
+    #model.set_variable('_x', 'd', shape=(1, 1))
+    model.set_variable('_x', 'v', shape=(1, 1))
+    model.set_variable('_x', 'Ie', shape=(1, 1))
 
-    u = model.set_variable('_u', 'u', shape=(1, 1))
-    #x = model.set_variable('_x', 'x', shape=(1, 1)) assuming 1D scenario else this is isn't needed
-    d = model.set_variable('_x', 'd', shape=(1, 1))
-    v = model.set_variable('_x', 'v', shape=(1, 1))
-
-    t = model.set_variable('_tvp', 't', shape=(1, 1)) # PINN input feature
-    v_prec = model.set_variable('_tvp', 'v_prec', shape=(1, 1))
+    model.set_variable('_tvp', 't', shape=(1, 1)) # PINN input feature
+    model.set_variable('_tvp', 'x_prec', shape=(1, 1))
+    model.set_variable('_tvp', 'v_prec', shape=(1, 1))
     
-    return h, d_min, u, v, d, t, v_prec
+    return model
 
-def _apply_kinematics_define_expr(
-    model: Model, 
-    v, a, d, d_min, h, v_prec):
+def policy_define_expr(
+    model: Model, a, d_min, h, L_prec):
     # Discrete‐kinematics update
-    """ dxdt = v 
-    model.set_rhs('x', dxdt) """
-    dgapdt = v_prec - v #gap
-    dvdt = a #vi
-    model.set_rhs('d', dgapdt)
-    model.set_rhs('v', dvdt)
+    x = model.x['x']
+    v = model.x['v']
+    x_prec = model.tvp['x_prec']
+    v_prec = model.tvp['v_prec']
 
-
+    d = x_prec - x - L_prec
     d_ref = d_min + h * v 
-    e = d - d_ref
-    ev = v_prec - v - h * a
-    E = ca.vertcat(e, ev)
+    error_spacing = d - d_ref
+    de = v_prec - v - h * a
+    Ie = model.x['Ie']
+    E = ca.vertcat(error_spacing, de, Ie)
     
+    error_rel_v = v_prec - v #for terminal cost
+    terminal_vec = ca.vertcat(error_spacing, error_rel_v, Ie)
+    
+
     model.set_expression('d_ref', d_ref)
     model.set_expression('d', d)
-    model.set_expression('e', e)
+    model.set_expression('e', error_spacing)
+    model.set_expression('e_rel_v', error_rel_v)
     model.set_expression('a', a) # For plotting/monitoring acceleration
     model.set_expression('E', E)
+    model.set_expression('E_term', terminal_vec)
 
+    return model
 
 def SecondOrderPINNmodel(onnx_model_path: str, const_params: dict,
                          scalerX_path: str = None, scalerY_path: str = None) -> Model: 
@@ -59,10 +64,14 @@ def SecondOrderPINNmodel(onnx_model_path: str, const_params: dict,
     #*Do-MPC model setup
     model = Model('continuous') #can be continous or discrete (have to handle discretization through euler or others)
 
-    h, d_min, u, v, d, t, v_prec = _set_model_common_params(model, const_params)
-    
-    d_ref = d_min + h*v
-    X = ca.horzcat(t, u, v, d, d_ref) #correct order of features
+    model = _set_model_common_params(model, const_params)
+    h = const_params["h"]
+    d_min = const_params["d_min"]
+    L_prec = const_params['L_prec']
+    d_ref = d_min + h*model.x['v']
+    d = model.tvp['x_prec'] - model.x['x'] - L_prec
+
+    X = ca.horzcat(model.tvp['t'], model.u['u'], model.x['v'], d, d_ref, model.tvp['v_prec']) #correct order of features
     if scalerX_path:
         scalerX = joblib.load(scalerX_path)
         target_min = scalerX.feature_range[0]
@@ -77,19 +86,134 @@ def SecondOrderPINNmodel(onnx_model_path: str, const_params: dict,
         scalerY = joblib.load(scalerY_path)
         a = a * scalerY.scale_ + scalerY.mean_  #in this case the scaler has single values/floats
 
-    _apply_kinematics_define_expr(model, v, a, d, d_min, h, v_prec)
+    model = policy_define_expr(model, a, d_min, h, L_prec)
+    dxdt = model.x['v']
+    model.set_rhs('x', dxdt)
+    #dgapdt = v_prec - v #gap
+    dvdt = a 
+    #model.set_rhs('d', dgapdt)
+    model.set_rhs('v', dvdt)
+    model.set_rhs("Ie", model.aux['e']) #set integral action
     
+    model.setup() #after this cannot setup more variables
+    return model
+
+def DiscreteWindowedPINN(onnx_model_path: str, const_params: dict,
+                         scalerX_path: str = None, scalerY_path: str = None) -> Model:
+    onnx_model = onnx.load(onnx_model_path)    
+    ca_converter = ONNXConversion(onnx_model)
+
+    #*Do-MPC model setup
+    model = Model('discrete')
+    
+    model = _set_model_common_params(model, const_params)
+
+    h = const_params["h"]
+    d_min = const_params["d_min"]
+    L_prec = const_params['L_prec']
+    dt = const_params['dt'] #since this one is discrete we need to perform integration manually
+
+    window_size = const_params['window_size']
+    X = ca.horzcat(model.tvp['t'], model.u['u'], model.x['v']) #*ensure correct order of features
+    n_features = 3
+
+    for i in range(1, window_size):
+        model.set_variable('_x', 't_km'+ str(i), shape=(1, 1)) 
+        model.set_variable('_x', 'v_km'+ str(i), shape=(1, 1))  #v[k-i]
+        model.set_variable('_x', 'u_km'+ str(i), shape=(1, 1))  #u[k-i]
+        old_feat = ca.horzcat(model.x['t_km'+ str(i)], model.x['u_km'+ str(i)], model.x['v_km'+ str(i)])
+        X = ca.vertcat(old_feat, X)
+
+    if scalerX_path:
+        scalerX = joblib.load(scalerX_path)
+        target_min = scalerX.feature_range[0]
+        #normalize manually, without breaking CASadi
+        scaleX = ca.DM(scalerX.scale_).reshape((1,-1))
+        minX = ca.DM(scalerX.min_).reshape((1,-1))
+        minX= ca.repmat(minX, window_size, 1) #replicate min value over window
+        scaleX = ca.repmat(scaleX, window_size, 1)
+        X = target_min + (X - minX) * scaleX
+
+    X = X.reshape((1, -1)) #flatten window
+
+    ca_converter.convert(input=X)
+    
+    a = ca_converter['output'] #get PINN prediction
+    if scalerY_path:
+        scalerY = joblib.load(scalerY_path)
+        a = a * scalerY.scale_ + scalerY.mean_  #in this case the scaler has single values/floats
+
+    model = policy_define_expr(model, a, d_min, h, L_prec)
+
+    v_next = model.x['v'] + a*dt
+    x_next = model.x['x'] + model.x['v']*dt + 0.5*a*dt**2
+    Ie_next = model.x['Ie'] + model.aux['e']*dt
+    model.set_rhs('v', v_next)
+    model.set_rhs('x', x_next)
+    model.set_rhs("Ie", Ie_next) #set integral action
+
+    for i in range(1, window_size):
+        if i==1:
+            model.set_rhs('t_km'+ str(i), model.tvp['t'])
+            model.set_rhs('v_km'+ str(i), model.x['v'])
+            model.set_rhs('u_km'+ str(i), model.u['u'])
+        else:
+            model.set_rhs('t_km'+ str(i), model.x['t_km'+ str(i-1)])
+            model.set_rhs('v_km'+ str(i), model.x['v_km'+ str(i-1)])
+            model.set_rhs('u_km'+ str(i), model.x['u_km'+ str(i-1)])
+
     model.setup() #after this cannot setup more variables
     return model
 
 def SecondOrderIdeal(const_params: dict) -> Model:
     model = Model('continuous') #can be continous or discrete
 
-    h, d_min, u, v, d, t, v_prec = _set_model_common_params(model, const_params)
+    model = _set_model_common_params(model, const_params)
+    h = const_params["h"]
+    d_min = const_params["d_min"]
+    L_prec = const_params['L_prec']
     m = const_params["m"]
-    a = u / m
 
-    _apply_kinematics_define_expr(model, v, a, d, d_min, h, v_prec)
-    
+    model = policy_define_expr(model, a, d_min, h, L_prec)
+
+    dxdt = model.x['v']
+    model.set_rhs('x', dxdt)
+    #dgapdt = v_prec - v #gap
+    a_target = model.u['u'] / m
+    dvdt = a_target 
+    #model.set_rhs('d', dgapdt)
+    model.set_rhs('v', dvdt)
+
+    model.set_rhs("Ie", model.aux['e']) #set integral action
+
     model.setup()
     return model
+
+def ThirdOrderModel(const_params: dict) -> Model:
+    model = Model('continuous')
+    h = const_params["h"]
+    d_min = const_params["d_min"]
+    L_prec = const_params['L_prec']
+    tau = const_params["tau"]
+    m = const_params["m"]
+    model = _set_model_common_params(model, const_params)
+    
+    a = model.set_variable('_x', 'a', shape=(1,1))
+    a_target = model.u['u']/m
+    dadt = 1/tau*(a_target - a) 
+    model.set_rhs('a', dadt)
+
+    model = policy_define_expr(model, a, d_min, h, L_prec)
+    
+    dxdt = model.x['v']
+    model.set_rhs('x', dxdt)
+    #dgapdt = v_prec - v #gap
+    dvdt = a 
+    #model.set_rhs('d', dgapdt)
+    model.set_rhs('v', dvdt)
+
+    model.set_rhs("Ie", model.aux['e']) #set integral action
+
+    model.setup()
+    return model
+    
